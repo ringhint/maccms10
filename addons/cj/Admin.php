83deef7051933b92e69324eccc8bdf7862a7a5f9
ringhint <47498421+ringhint@users.noreply.github.com>
2023-02-10 19:07:24 +0800

    Delete addons/cj directory

--- addons/cj/Admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
-Admin采集插件中心 版权所有 ：www.admincj.com
-**/
-namespace addons\admin;use think\Addons;class admin extends Addons{public function install(){return true;}public function uninstall(){return true;}}
